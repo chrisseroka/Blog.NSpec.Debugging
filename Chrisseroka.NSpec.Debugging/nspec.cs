24df1f106bea28f68d9ee28ddc3f64966a8edcd1
chrisseroka <k.s.seroka@gmail.com>
2014-11-15 08:07:59 +0100

    Initial commit

---
+++ Chrisseroka.NSpec.Debugging/nspec.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using NSpec;
+using NSpec.Domain;
+using NSpec.Domain.Formatters;
+using NUnit.Framework;
+
+namespace Chrisseroka.NSpec.Debugging
+{
+    [TestFixture]
+    public abstract class nspec: global::NSpec.nspec
+    {
+        [Test]
+        public void debug()
+        {
+            var currentSpec = this.GetType();
+            var finder = new SpecFinder(new[] {currentSpec});
+            var builder = new ContextBuilder(finder, new Tags().Parse(currentSpec.Name), new DefaultConventions());
+            var runner = new ContextRunner(builder, new ConsoleFormatter(), false);
+            var results = runner.Run(builder.Contexts().Build());
+
+            //assert that there aren't any failures
+            results.Failures().Count().should_be(0);
+        }
+    }
+}
